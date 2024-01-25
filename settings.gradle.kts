937895e4f9c5e8535f7e7a592d4511324187c65e
Matthew Mifsud <mmifsud1999@gmail.com>
2024-01-25 13:46:59 -0600

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mifsud CS 408 Lab #1A Mystery Helper"
+include(":app")
