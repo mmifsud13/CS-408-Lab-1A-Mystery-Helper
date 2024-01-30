eb51bc84da8081a66f7762caf0e6ac66cc667061
Matthew Mifsud <mmifsud1999@gmail.com>
2024-01-30 15:32:23 -0600

    Final Commit

--- app/src/main/java/com/example/mifsudcs408lab1amysteryhelper/MainActivity.java
+++ app/src/main/java/com/example/mifsudcs408lab1amysteryhelper/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.mifsudcs408lab1amysteryhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.mifsudcs408lab1amysteryhelper.databinding.ActivityMainBinding;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Button button = findViewById(R.id.button);
+        TextView textview = findViewById(R.id.outputText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] array = getResources().getStringArray(R.array.outputs);
+                String randomStr = array[new Random().nextInt(array.length)];
+                textview.setText(randomStr);
+            }
+        });
     }
 }
